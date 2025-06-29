ae8a48739ad53f606df9734c55f8b7c62e6fa1ec
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-06-29 18:33:55 +0530

    initial navbar components added

--- parish360-ui/src/app/panels/navbar/navbar.component.ts
+++ parish360-ui/src/app/panels/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { faBars, faBell } from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  faBars = faBars
+  faBell = faBell
 }
