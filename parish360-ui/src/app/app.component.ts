d9df94eefd627331334d48757f5aa26e80e4eec3
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-06-29 16:11:21 +0530

    initial layout changes

--- parish360-ui/src/app/app.component.ts
+++ parish360-ui/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./panels/navbar/navbar.component";
+import { SidebarComponent } from "./panels/sidebar/sidebar.component";
+import { MainComponent } from "./panels/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
