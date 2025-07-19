96fa622f00023f16a71e7603fb58345206061cfa
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-07-19 13:04:36 +0530

    design changes to sidenav

--- parish360-ui/src/app/panels/sidebar/sidebar.component.ts
+++ parish360-ui/src/app/panels/sidebar/sidebar.component.ts
@@ -1,18 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBook,
+  faCalendarCheck,
+  faFileInvoiceDollar,
+  faLayerGroup,
+  faMoneyBillWave,
+  faPeopleGroup,
+  faPeopleRoof,
+  faUsersCog,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
   navItems = [
-    { label: 'Parish Dashboard', icon: 'view_grid', route: 'dashboard' },
-    { label: 'Family Records', icon: 'person', route: 'family-records' },
-    { label: 'Bookings', icon: 'settings', route: 'bookings' },
+    { label: 'Parish Dashboard', icon: faLayerGroup, route: 'dashboard' },
+    { label: 'Family Records', icon: faPeopleRoof, route: 'family-records' },
+    { label: 'Bookings', icon: faCalendarCheck, route: 'bookings' },
+    { label: 'Ceremony Register', icon: faBook, route: 'ceremony' },
+    { label: 'Pious Associations', icon: faPeopleGroup, route: 'associations' },
+    { label: 'Payment Register', icon: faFileInvoiceDollar, route: 'payments' },
+    { label: 'Expense Register', icon: faMoneyBillWave, route: 'expenses' },
+    { label: 'Users', icon: faUsersCog, route: 'users' },
   ];
 }
