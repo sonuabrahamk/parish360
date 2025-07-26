5af638d630da68bd42918d91fcd7fa0ceacd6a6c
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-07-26 10:53:39 +0530

    initial project created for parish360 core application

---
+++ parish360-core/src/main/java/org/parish360/core/Parish360CoreApplication.java
@@ -0,0 +1,11 @@
+package org.parish360.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parish360CoreApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(Parish360CoreApplication.class, args);
+    }
+}
