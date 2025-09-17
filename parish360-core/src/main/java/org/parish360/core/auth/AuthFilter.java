fe7f152e4d1c9b43eec4a102067d1241a488a5fe
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-09-17 11:51:03 +0530

    fixing auth filter exception handling

--- parish360-core/src/main/java/org/parish360/core/auth/AuthFilter.java
+++ parish360-core/src/main/java/org/parish360/core/auth/AuthFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import org.parish360.core.common.util.JwtUtil;
+import org.parish360.core.error.exception.AccessDeniedException;
 import org.parish360.core.error.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -22,6 +24,9 @@ public class AuthFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
@@ -42,24 +47,29 @@ public class AuthFilter extends OncePerRequestFilter {
                     if (path == null) {
                         throw new ResourceNotFoundException("invalid endpoint");
                     }
-                    if (token != null
-                            && SecurityContextHolder.getContext().getAuthentication() == null
-                            && jwtUtil.validateToken(token, path, request.getMethod())) {
-                        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                                jwtUtil.extractSubject(token), null, null);
+                    try {
+                        if (token != null
+                                && SecurityContextHolder.getContext().getAuthentication() == null
+                                && jwtUtil.validateToken(token, path, request.getMethod())) {
+                            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                                    jwtUtil.extractSubject(token), null, null);
 
-                        // This is CRUCIAL
-                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                            // This is CRUCIAL
+                            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                            SecurityContextHolder.getContext().setAuthentication(authToken);
 
-                        // refresh cookie and add to response
-                        Cookie refreshCookie = new Cookie(AuthConstants.AUTH_COOKIE_NAME, token);
-                        refreshCookie.setHttpOnly(true);
-                        refreshCookie.setSecure(true);
-                        refreshCookie.setPath("/");
-                        refreshCookie.setMaxAge(AuthConstants.COOKIE_EXPIRY_SECONDS);
-                        response.addCookie(refreshCookie);
+                            // refresh cookie and add to response
+                            Cookie refreshCookie = new Cookie(AuthConstants.AUTH_COOKIE_NAME, token);
+                            refreshCookie.setHttpOnly(true);
+                            refreshCookie.setSecure(true);
+                            refreshCookie.setPath("/");
+                            refreshCookie.setMaxAge(AuthConstants.COOKIE_EXPIRY_SECONDS);
+                            response.addCookie(refreshCookie);
+                        }
+                    } catch (Exception e) {
+                        handlerExceptionResolver.resolveException(request, response, null, new AccessDeniedException(e.getMessage()));
+                        return;
                     }
                 }
             }
