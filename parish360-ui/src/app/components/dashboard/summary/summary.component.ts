198522ac38f3203c46ff64149a8470c08e3901e8
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-10-30 10:04:26 +0530

    summary section UI changes

--- parish360-ui/src/app/components/dashboard/summary/summary.component.ts
+++ parish360-ui/src/app/components/dashboard/summary/summary.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Summary } from '../../../services/interfaces/dashboard.interface';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faPeopleGroup,
+  faPeopleRoof,
+  faUsers,
+  faUsersRectangle,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.css'
+  styleUrl: './summary.component.css',
 })
 export class SummaryComponent {
   @Input() summary!: Summary;
+
+  faPeopleGroup = faPeopleGroup;
+  faUsersRectangle = faUsersRectangle;
+  faPeopleRoof = faPeopleRoof;
+  faUsers = faUsers;
 }
