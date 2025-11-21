1c974c953d31b2dea89a532e2624049b4fb7ebb7
Sonu Abraham <sonu.abrahamk@gmail.com>
2025-11-21 19:27:54 +0530

    increasing cookie expiration to 1hour

--- parish360-core/src/main/java/org/parish360/core/auth/AuthConstants.java
+++ parish360-core/src/main/java/org/parish360/core/auth/AuthConstants.java
@@ -2,7 +2,7 @@ package org.parish360.core.auth;
 
 public class AuthConstants {
     public static final String AUTH_COOKIE_NAME = "auth-cookie";
-    public static final int COOKIE_EXPIRY_SECONDS = 180;
+    public static final int COOKIE_EXPIRY_SECONDS = 3600;
     public static final long COOKIE_EXPIRY_MINUTES = 60;
     public static final String TOKEN_PERMISSIONS = "permissions";
     public static final String LOCALE = "locale";
